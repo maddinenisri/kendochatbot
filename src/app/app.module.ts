7b229cfddbf0df5d3eb3f3780d38bd2ca2f10364
Srini Maddineni <srini@mdstechinc.com>
2018-06-04 07:47:21 -0400

    Initial integration using KendoUI and Angular with Diaglogflow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ButtonModule } from '@progress/kendo-angular-buttons';
+import { ChatModule } from '@progress/kendo-angular-conversational-ui';
 
 import { AppComponent } from './app.component';
+import { PaymentPlanCardComponent } from './payment-plan-card.component';
+import { QuoteCardComponent } from './quote-card.component';
 
 @NgModule({
+  imports:      [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    ChatModule
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentPlanCardComponent,
+    QuoteCardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
+
